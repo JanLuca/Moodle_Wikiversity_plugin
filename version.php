48f2ee9c75b9763bb8f593951ad72eb8128dfac8
JanLuca <jan@naumannsfamily.de>
2013-04-16 14:39:13 +0200

    Add version.php

---
+++ version.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Include Wikiversity-pages into Moodle
+ *
+ * @package    filter
+ * @subpackage wikiversity
+ * @copyright  2013 by Jan Luca Naumann
+ * @author     Jan Luca Naumann <jan@jans-seite.de>
+ * @license    CC-BY-SA 3.0 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version  = 2013041600;
+$plugin->component= 'filter_wikiversity';
+$plugin->release = '1.0 (Build: 2013041600)'; 
