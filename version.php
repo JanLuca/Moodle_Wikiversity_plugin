bf76e60664df5c78c374ad4c2c520ba7ce1d6830
JanLuca <jan@naumannsfamily.de>
2013-04-18 21:58:25 +0200

    Update version.php

--- version.php
+++ version.php
@@ -26,7 +26,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2013041800;
-$plugin->component= 'filter_wikiversity';
-$plugin->release = '1.0 (Build: 2013041600)'; 
-$plugin->requires = 2012062500;
+$plugin->version   = 2013041800;
+$plugin->component = 'filter_wikiversity';
+$plugin->release   = '1.0 (Build: 2013041800)'; 
+$plugin->requires  = 2012062500;
+$plugin->cron      = 0;
+$plugin->maturity  = MATURITY_STABLE;
